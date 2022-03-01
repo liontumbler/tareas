87221270a4e6ee2245177483de1f2722313b4fc8
liontumbler <liontumbler@gmail.com>
2022-03-01 13:33:16 -0500

    se crea otro problema resuelto llamado ejercicio 3

---
+++ ejercicio3.php
@@ -0,0 +1,41 @@
+<?php
+/*
+10.Genere el código en php para imprimir toda la matriz, entregue un archivo con el nombre “pregunta12.php”
+
+<?php
+
+$MatrizNumerica = array(
+    array(1, 2, 3, 4, 5),
+    array(1, 2, 3, 4, 5),
+    array(1, 2, 3, 4, 5),
+    array(1, 2, 3, 4, 5)
+);
+La función debe imprimir lo siguiente:
+Total: 4
+Total sub array: 5
+12345
+12345
+12345
+12345
+*/
+$MatrizNumerica = array(
+    array(1, 2, 3, 4, 5),
+    array(1, 2, 3, 4, 5),
+    array(1, 2, 3, 4, 5),
+    array(1, 2, 3, 4, 5)
+);
+
+$total = count($MatrizNumerica);
+
+echo 'Total: '.$total;
+echo '<hr>';
+
+$cadena='';
+for ($i=0; $i < $total; $i++) { 
+    echo 'campo '.$i.' sub array: '.count($MatrizNumerica[$i]);
+    echo '<br>';
+    $cadena .= implode("", $MatrizNumerica[$i]).'<br>';
+}
+
+echo '<hr>';
+echo $cadena;
