0f20b9429c56dea155570a1614f9c4f451b6a5b9
liontumbler <liontumbler@gmail.com>
2022-03-01 13:30:59 -0500

    se crea otro problema resuelto llamado ejercicio 2

---
+++ ejercicio2.php
@@ -0,0 +1,25 @@
+<?php
+/*
+6.Escriba el código necesario para inicializar el siguiente arreglo de datos: entregue un archivo “pregunta7.php” QUE AL EJECUTARLO MUESTRE LA SIGUIENTE IMPRESIÓN 
+
+$arreglo = Array
+(
+	[1] => '001'
+	[2] => '002'
+	...
+	[100] => '100'
+)
+*/
+$array = array();
+for ($i=1; $i < 101; $i++) { 
+
+    if($i > 9 && $i <= 99)
+        $array[$i] = '0'.$i;
+    elseif($i > 99)
+        $array[$i] = $i;
+    else
+        $array[$i] = '00'.$i;
+    
+}
+
+print_r($array);
